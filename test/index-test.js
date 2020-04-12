a5eed071b8a906d499600e2a13deae9d44c949d6
Preston Phelps <p_phelps09@hotmail.com>
2020-04-12 13:43:28 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn("WARNING!")', done => {
+  it('calls console.warn("HALP")', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
